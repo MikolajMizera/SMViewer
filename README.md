dc2638195272b526ef0a5ab339c1ed392d46f607
MikolajMizera <mikolajmizera@gmail.com>
2020-05-07 15:34:18 +0200

    Added docstrings and example files

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # SMViewer
 Simple Molecules Viewer is a Python Dash app for displaying sets of molecules 
 and filtering them by a property.
+## Getting Started
+### Dependencies
+SMViewer requires:
+- dash
+- rdkit
+- yaml
+- numpy
+- pandas
+### Installing
+The main and only required file is `app.py` file. The user needs to specify
+`config.yaml` (see examples) and provide SDF file with chemical structures and
+properties. The properties should include numerical value to filter by, 
+template address to external database, and ID in the database. Please refer to
+`app.py` docstring for detailed information.
+## Running the examples
+For running example on development server, please run app.py script from the 
+same directory as the example files.
+```
+python app.py
+```
+For production deployment gunicorn is advised (Linux-only).
+```
+pip install gunicorn
+```
+```
+gunicorn app:server
+```
--- app.py
+++ app.py
@@ -1,52 +1,97 @@
-import os
-from os.path import join, split
-import sys
-print(sys.version)
-import numpy as np
-import flask
-import yaml
+"""
+The script with Dash application which displays a set of molecular structures
+and allows filtering by a specified value using a slider.
 
-from tqdm import tqdm
-import dash
-print(dash.__file__)
-import dash_core_components as dcc
+The app is configurable through YAML config files. Configuration parameters
+include:
+
+    molecules_sdf: string
+    Defines a path to an SDF file with molecular structures to be displayed.
+
+    properties: list of strings
+    It contains a list of properties in an SDF which will be used as a
+    parameter to filter by. To access view for the selected parameter, the user
+    will go to a specific web page. For parameter `pKi` it would be
+    `localhost:8050/pKi`.
+
+    db_links: disctionary
+    A dictionary with property name as a key and link to database with a
+    `%s` placeholder for id.
+    Example: for ChEMBL https://www.ebi.ac.uk/chembl/compound_report_card/%s/
+    would be used, where `%s` will be replaced by molecule-specific id (see
+    below).
+
+    db_id_props: dictionary
+    A dictionary with property name as a key and id of corresponding  structure
+    in an external database as a value. Will be inserted into the database link
+    according to the specified template (see above).
+
+"""
+
+from os.path import join, split
+
+import numpy as np
 import pandas as pd
-import dash_html_components as html
-from dash.dependencies import Input, Output
+import yaml
+from tqdm import tqdm
 from base64 import b64encode
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
 from rdkit.Chem import Draw
 from rdkit import Chem
 
-def PrintAsBase64PNGString(mol, highlightAtoms=[], molSize=(200, 200)):
-    data = Draw._moltoimg(mol, molSize, highlightAtoms, '', returnPNG=True,
+__author__ = 'Mikołaj Mizera'
+__copyright__ = 'Copyright 2020, SMViewer'
+__license__ = 'GNU General Public License v3.0'
+__version__ = '0.0.1'
+__maintainer__ = 'Mikołaj Mizera'
+__email__ = 'mikolajmizera@gmail.com'
+__status__ = 'PROTOTYPE'
+
+
+def PrintAsBase64PNGString(mol, legend='', highlightAtoms=[],
+                           molSize=(200, 200)):
+    """A helper function to depict a moelcular structure and encode it as a
+    base64 string."""
+
+    data = Draw._moltoimg(mol,
+                          molSize,
+                          highlightAtoms,
+                          legend,
+                          returnPNG=True,
                           kekulize=True)
     return b64encode(data).decode('ascii')
+
 
 app = dash.Dash(__name__)
 server = app.server
 
+# Get execution parmeters from the config.yaml file
 config = yaml.safe_load(open('config.yaml'))
 mol_file = config['molecules_sdf']
 properties = config['properties']
 db_links = config['db_links']
 db_id_props = config['db_id_props']
 
+# Read all properties from SDF file
 mols = [m for m in Chem.SDMolSupplier(mol_file, removeHs=True)]
-df = pd.DataFrame([m.GetPropsAsDict() for m in tqdm(mols)])
-df = df.assign(mol=mols)
+pbar = tqdm(mols, desc='Loading structures from SDF')
+df = pd.DataFrame([m.GetPropsAsDict() for m in pbar]).assign(mol=mols)
 df[properties] = df[properties].replace('N/A', np.nan)
 
-print('Rendering structures...')
+# Creates 2D depictions of moelcules and stores them for faster loading
 try:
     imgs = np.load('imgs_backup.npy')
-except Exception as e:
-    print(e)
-    imgs = [PrintAsBase64PNGString(m) for m in tqdm(df.mol.values)]
+except FileNotFoundError:
+    pbar = tqdm(df.mol.values, desc='Rendering 2D structures')
+    imgs = [PrintAsBase64PNGString(m) for m in pbar]
     np.save('imgs_backup', imgs)
-
 df = df.assign(imgs=imgs)
 
-# the slider with initial settings, which will be updated dynamically
+# Settings of slider will be updated accoridng to values of selected property
 slider = dcc.RangeSlider(id='slider',
                          min=0,
                          max=10,
@@ -55,7 +100,7 @@ slider = dcc.RangeSlider(id='slider',
                          marks={str(n): {'label': '%.2f'%n}
                                 for n in np.arange(11)})
 
-# header contains slider and buttion
+# Layout
 header = [
     html.Tr(
             html.Td(
@@ -76,7 +121,7 @@ header = [
                                 id='button',
                                 style={'width': '15%',
                                        'margin-top': '2.5em'}),
-                        style={'width': '85%', 
+                        style={'width': '85%',
                                'margin-top': '2.5em'}),
                 html.Td([
                     html.A(html.Img(src=join('assets', 'github_icon.png'),
@@ -95,23 +140,22 @@ header = [
 
 
 app.layout = html.Div(
-    [
-    dcc.Location(id='url', refresh=False),
-    html.Table(header, style={'width': '100%'}),
-    dcc.Loading(
-                html.Table([],
-                           id='results',
-                           style={'width': '100%',
-                                  'border-spacing': '1em',
-                                  'margin-top': '2em',
-                                  'border-top-width': '1px',
-                                  'border-top-style': 'dashed',
-                                  'border-bottom-style': 'dashed',
-                                  'border-top-color': 'grey'})
+    [dcc.Location(id='url', refresh=False),
+     html.Table(header, style={'width': '100%'}),
+     dcc.Loading(
+             html.Table([],
+                        id='results',
+                        style={'width': '100%',
+                               'border-spacing': '1em',
+                               'margin-top': '2em',
+                               'border-top-width': '1px',
+                               'border-top-style': 'dashed',
+                               'border-bottom-style': 'dashed',
+                               'border-top-color': 'grey'})
                 )
     ])
 
-# Updaters - check if url points to a property and update UI accrodingly
+# Updaters - update UI elements accroding to property name
 @app.callback(dash.dependencies.Output('slider', 'marks'),
               [dash.dependencies.Input('url', 'pathname')])
 def update_slider_marks(pathname):
@@ -135,9 +179,8 @@ def update_slider_min(pathname):
             return df[prop].dropna().min()
     return 0
 
-@app.callback(
-dash.dependencies.Output('slider', 'max'),
-[dash.dependencies.Input('url', 'pathname')])
+@app.callback(dash.dependencies.Output('slider', 'max'),
+              [dash.dependencies.Input('url', 'pathname')])
 def update_slider_max(pathname):
 
     if not (pathname is None):
@@ -146,9 +189,8 @@ def update_slider_max(pathname):
             return df[prop].dropna().max()
     return 10
 
-@app.callback(
-dash.dependencies.Output('slider', 'value'),
-[dash.dependencies.Input('url', 'pathname')])
+@app.callback(dash.dependencies.Output('slider', 'value'),
+              [dash.dependencies.Input('url', 'pathname')])
 def update_slider_value(pathname):
 
     if not (pathname is None):
@@ -157,21 +199,18 @@ def update_slider_value(pathname):
             return [df[prop].dropna().min(), df[prop].dropna().max()]
     return [0,10]
 
-@app.callback(
-dash.dependencies.Output('results', 'children'),
-[dash.dependencies.Input('button', 'n_clicks')],
-[dash.dependencies.State('slider', 'value'),
-  dash.dependencies.State('url', 'pathname')])
+
+@app.callback(dash.dependencies.Output('results', 'children'),
+              [dash.dependencies.Input('button', 'n_clicks')],
+              [dash.dependencies.State('slider', 'value'),
+               dash.dependencies.State('url', 'pathname')])
 def update_table(n_clicks, prop_range, pathname):
 
     if (not n_clicks) or (pathname is None):
         return []
 
-    pathname = split(pathname)[-1]
-    #check if pathname refers to any known property, otherwise use defaults
-    if pathname in properties:
-        prop = pathname
-    else:
+    prop = split(pathname)[-1]
+    if not (prop in properties):
         return []
 
     prop_min, prop_max = prop_range
@@ -180,12 +219,14 @@ def update_table(n_clicks, prop_range, pathname):
     db_ids_prop = db_id_props[prop]
     df_prop = df.dropna(subset=[prop])
 
-    mask = (df_prop[prop]>=prop_min) & (df_prop[prop]<=prop_max)
+    mask = (df_prop[prop] >= prop_min) & (df_prop[prop] <= prop_max)
     # limit display to 250 images
+    # TODO: add proper configuration parameter
     df_prop = df_prop[mask].sort_values(by=prop).iloc[:250]
 
     imgs = [html.Img(src='data:image/png;base64,%s'%img)
             for img in df_prop.imgs]
+
     labels = [html.P(['%s: %.2f'%(prop, pki),
                       html.Br(),
                       html.A('%s'%chid,
@@ -194,11 +235,13 @@ def update_table(n_clicks, prop_range, pathname):
                      style={'text-align': 'center'})
               for chid, pki in zip(df_prop[db_ids_prop].values,
                                    df_prop[prop].values)]
+
     tds = [html.Td([html.Tr(i), html.Tr(l)]) for i, l in zip(imgs, labels)]
     inds = np.array_split(np.arange(len(tds)), int(np.ceil(len(tds)/4)))
     trs = [html.Tr([tds[i] for i in ind]) for ind in inds]
 
     return trs
+
 
 if __name__ == '__main__':
     app.run_server(debug=False)
